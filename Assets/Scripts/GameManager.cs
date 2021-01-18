b47498844b328cafa933ea72225df1fff384ae9e
loopingleaf <luc.lauriou@live.fr>
2021-01-18 22:29:11 +0100

    Add possibility to restart

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,22 +5,37 @@ using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    private int hp = 7;
-    private int ennemyHp = 8;
     public int Hp { get { return hp; } set { hp = Mathf.Max(0, value); } }
     public int EnnemyHp { get { return ennemyHp; } set { ennemyHp = Mathf.Max(0, value); } }
+    private int hp = 7;
+    private int ennemyHp = 8;
+    private bool isRestart = false;
 
     public void DisplayInfo(TextMeshProUGUI tm)
     {
+        if(isRestart)
+        {
+            hp = 7;
+            ennemyHp = 8;
+            tm.text = "Okay, let's try again";
+            isRestart = false;
+        }
         tm.text += "\nHPs: " + Hp + " - Ennemy's HPs: " + EnnemyHp;
         if(hp <= 0)
         {
             tm.text += "\nYou are dead...";
+            AskForRestart(tm);
         }
         else if (ennemyHp <= 0)
         {
-            tm.text += "\nYou win !";
+            tm.text = "You win !";
+            AskForRestart(tm);
         }
     }
 
+    private void AskForRestart(TextMeshProUGUI tm)
+    {
+        tm.text += "\nPress a button to restart";
+        isRestart = true;
+    }
 }
