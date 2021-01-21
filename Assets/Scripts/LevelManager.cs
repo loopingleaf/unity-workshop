814c6b3658a35494b1c7730e16cf28f2adce9ce6
loopingleaf <luc.lauriou@live.fr>
2021-01-21 17:29:26 +0100

    Start to make dungeon crawler

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public int maxNbLevels;
+    private List<Room> dungeon;
+
+    void Start()
+    {
+        GenerateDungeon();
+    }
+
+    private void GenerateDungeon()
+    {
+        int nbDirections = Enum.GetNames(typeof(Room.Direction)).Length;
+        for(int i = 0; i < maxNbLevels; i++)
+        {
+            Room[] roomTypeList = GenerateRoomTypes(nbDirections);
+            foreach(Room.Direction dir in Enum.GetValues(typeof(Room.Direction)))
+            {
+                new Room(i, )
+            }
+        }
+    }
+
+    private Room[] GenerateRoomTypes(int size)
+    {
+        Room[] retval = new Room[size];
+        string fightRoomText = "An monster !";
+        string treasureRoomText = "A treasure !";
+        string fightTreasureRoom = "An treasure... guarded by a monster !";
+        // TODO
+        return retval;
+    }
+}
---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room
+{
+    public enum Direction { Left, Front, Right };
+
+    public int level;
+    public string onEnterText;
+    public Item treasure;
+    public Enemy enemy;
+    public Room Parent {
+        get {return parent;}
+        set {
+            if(parent != null)
+                Debug.Log("WARNING: Parent of room already set");
+            parent = value;
+        }
+    }
+    private Room parent;
+    private Dictionary<Room, Direction> childs = new Dictionary<Room, Direction>();
+
+    public Room(string onEnterText, Enemy enemy = null, Item treasure = null)
+    {
+        this.onEnterText = onEnterText;
+        this.enemy = enemy;
+        this.treasure = treasure;
+    }
+
+    public void AddChild(Room room, Direction direction)
+    {
+        room.parent = this;
+        childs.Add(room, direction);
+    }
+}
